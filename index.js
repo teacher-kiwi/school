b1ba4ba2b2f46d696f741ac236c4b9b7e9bb04f1
teacher-kiwi <kwi07@hanmail.net>
2021-04-06 21:59:34 +0900

    update

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 // const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('과연 될까?')
-})
+// app.get('/', (req, res) => {res.send('과연 될까?')})
+app.get('/', function(req, res){
+  return res.send('이것도 될까?')
+});
 
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`)
