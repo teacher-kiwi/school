0eeceb7b6736d935e9e2bf7ba90a5129ecee5b69
teacher-kiwi <kwi07@hanmail.net>
2021-04-10 14:28:57 +0900

    update

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 // const port = 3000
 
-const http = require("http");
-setInterval(function () {
-  http.get("https://kiwi-school.herokuapp.com");
-}, 600000);
+const template = `오 신기하구만`
+
+
+
 
 
 // app.get('/', (req, res) => {res.send('과연 될까?')})
 app.get('/', function(req, res){
-  return res.send('이것도 될까?ㅋㅋㅋ')
+  return res.send(template);
 });
-
-
 
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`)
