7ac604835b9f5f6f37fb1f5f5219a19a547ae099
teacher-kiwi <kwi07@hanmail.net>
2021-11-28 00:43:11 +0900

    테스트

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +14,39 @@ app.get("/", (req, res) => {
 app.get("/school", (req, res) => {
   res.sendFile(`${__dirname}/school.html`);
 });
+
+const startApolloServer = async () => {
+  const data = require("./db");
+
+  const typeDefs = `
+  type Data {
+    SCHUL_NM: String
+    SCHUL_RDNMA: String
+    LTTUD: String
+    LGTUD: String
+  }
+
+  type Query {
+    school(name: String): [Data]
+  }
+    `;
+
+  const resolvers = {
+    Query: {
+      school: (_, arg) => {
+        return data.list.filter((element) =>
+          element.SCHUL_NM.includes(arg.name)
+        );
+      },
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+
+  app.use(server.getMiddleware({ path: "/graphql" }));
+  //server.applyMiddleware({ app, path: "/graphql" });
+};
+
+startApolloServer();
 app.listen(process.env.PORT || 3000);
