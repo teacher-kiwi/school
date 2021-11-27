e865685509af59d7701562a7a179d477b39514ea
teacher-kiwi <kwi07@hanmail.net>
2021-11-28 00:39:07 +0900

    테스트

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.sendFile(`${__dirname}/index.html`);
 });
 
+app.get("/school", (req, res) => {
+  res.sendFile(`${__dirname}/school.html`);
+});
 app.listen(process.env.PORT || 3000);
