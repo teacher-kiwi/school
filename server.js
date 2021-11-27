f25a524f77f7032b5346f294d06c56631353e661
teacher-kiwi <kwi07@hanmail.net>
2021-11-28 01:04:23 +0900

    테스트

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
-const { ApolloServer } = require("apollo-server-express");
-const express = require("express");
-const app = express();
-const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const data = require("./db");
 
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.get("/school", (req, res) => {
-  res.sendFile(`${__dirname}/school.html`);
-});
-
-const startApolloServer = async () => {
-  const data = require("./db");
-
-  const typeDefs = `
+const typeDefs = `
   type Data {
     SCHUL_NM: String
     SCHUL_RDNMA: String
@@ -31,21 +15,13 @@ const startApolloServer = async () => {
   }
     `;
 
-  const resolvers = {
-    Query: {
-      school: (_, arg) => {
-        return data.list.filter((element) =>
-          element.SCHUL_NM.includes(arg.name)
-        );
-      },
+const resolvers = {
+  Query: {
+    school: (_, arg) => {
+      return data.list.filter((element) => element.SCHUL_NM.includes(arg.name));
     },
-  };
-
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-
-  server.applyMiddleware({ app, path: "/graphql" });
+  },
 };
 
-startApolloServer();
-app.listen(process.env.PORT || 3000);
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen({ port: process.env.PORT || 4000 });
