59f6e833cb4cd781dea6b8e27e5c70a553e4015b
teacher-kiwi <kwi07@hanmail.net>
2021-11-28 08:56:15 +0900

    테스트

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ const startApolloServer = async () => {
     },
   };
 
-  const server = new ApolloServer({ typeDefs, resolvers, playground: true });
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
 
   app.use(server.getMiddleware({ path: "/graphql" }));
