ef74b0588aa0ff54b864bab61a03965b801c120e
teacher-kiwi <kwi07@hanmail.net>
2021-11-28 08:32:52 +0900

    테스트

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const app = express();
 
-const data = require("./db");
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/school.html`);
+});
 
-const typeDefs = `
-  type Data {
-    SCHUL_NM: String
-    SCHUL_RDNMA: String
-    LTTUD: String
-    LGTUD: String
-  }
-
-  type Query {
-    school(name: String): [Data]
-  }
-    `;
-
-const resolvers = {
-  Query: {
-    school: (_, arg) => {
-      return data.list.filter((element) => element.SCHUL_NM.includes(arg.name));
-    },
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-server.listen({ port: process.env.PORT || 3000 });
+app.listen(process.env.PORT || 3000);
